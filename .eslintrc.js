282a34b290f16796e1492c09bd8f04c7bf5a2e51
Miten Chauhan <miten@chauhan.nz>
2023-03-08 09:26:45 +1100

    Install eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  extends: ["cloud-party"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./apps/*/tsconfig.json"],
+  },
   settings: {
     next: {
       rootDir: ["apps/*/"],
