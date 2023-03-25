93168fb637f815f7cd2afcadcdd136b6dd61889d
Miten Chauhan <miten@chauhan.nz>
2023-03-25 12:02:50 +1100

    Integrate flagsmith

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,9 +1,37 @@
-import { type AppType } from "next/app";
+import { type AppProps, type AppType } from "next/app";
+import flagsmith from "flagsmith/isomorphic";
+import { FlagsmithProvider } from "flagsmith/react";
+import { type IState } from "flagsmith/types";
 
+import { env } from "@/env.mjs";
 import "../styles/globals.css";
 
-const App: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+type Props = {
+  flagsmithState?: IState;
+};
+
+const App: AppType = ({
+  Component,
+  pageProps,
+  flagsmithState,
+}: AppProps & Props) => {
+  return (
+    <FlagsmithProvider
+      serverState={flagsmithState}
+      options={{ environmentID: env.NEXT_PUBLIC_FLAGSMITH_ENV_KEY }}
+      flagsmith={flagsmith}
+    >
+      <Component {...pageProps} />
+    </FlagsmithProvider>
+  );
+};
+
+App.getInitialProps = async () => {
+  await flagsmith.init({ environmentID: env.NEXT_PUBLIC_FLAGSMITH_ENV_KEY });
+
+  return {
+    flagsmithState: flagsmith.getState(),
+  };
 };
 
 export default App;
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,17 +1,25 @@
 import { type NextPage } from "next";
+import { useFlags } from "flagsmith/react";
 
 import { Button } from "@cloud-party/ui";
 
 const Web: NextPage = () => {
+  const flags = useFlags(["proof_of_concept"]);
+
+  const { enabled: proofOfConceptEnabled, value: flagValue } =
+    flags.proof_of_concept;
+
   return (
     <div className="h-screen w-screen bg-slate-600">
       <h1 className="text-9xl font-bold">Web</h1>
-      <Button
-        logger="testing this thing"
-        className="my-test-class font-thin uppercase"
-      >
-        Lets go world
-      </Button>
+      {proofOfConceptEnabled && (
+        <>
+          <Button className="my-test-class font-thin uppercase">
+            Lets go world
+          </Button>
+          <code>{flagValue}</code>
+        </>
+      )}
     </div>
   );
 };
