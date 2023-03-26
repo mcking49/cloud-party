b540c8881ecda426b3b9655739b0950b64d5c8c6
Miten Chauhan <miten@chauhan.nz>
2023-03-26 22:09:13 +1100

    Remove signin buttons if signup is disabled

--- apps/web/src/pages/sign-in/[[...index]].tsx
+++ apps/web/src/pages/sign-in/[[...index]].tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import { SignIn } from "@clerk/nextjs";
@@ -10,6 +11,28 @@ const SignInPage: NextPage = () => {
   const redirectUrl = (query.redirect_url as string) ?? "/";
   const { enabled: signUpEnabled } = useFeatureFlag("sign_up");
 
+  // Remove elements from the dom if signup is disabled.
+  // This is not a full solution, but it's a start.
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      const dividerRow = document.querySelector(".cl-dividerRow");
+      const footerActionSignIn = document.querySelector(".cl-footer");
+      const socialButtons = document.querySelector(".cl-socialButtons");
+
+      if (dividerRow) {
+        dividerRow.remove();
+      }
+
+      if (footerActionSignIn) {
+        footerActionSignIn.remove();
+      }
+
+      if (socialButtons) {
+        socialButtons.remove();
+      }
+    }
+  }, [signUpEnabled]);
+
   return (
     <SignIn
       path="/sign-in"
@@ -19,7 +42,9 @@ const SignInPage: NextPage = () => {
       appearance={{
         elements: {
           // NOTE: this doesn't remove the link from the DOM.
-          footerAction__signIn: clsx({ hidden: !signUpEnabled }),
+          dividerRow: clsx({ hidden: !signUpEnabled }),
+          footer: clsx({ hidden: !signUpEnabled }),
+          socialButtons: clsx({ hidden: !signUpEnabled }),
         },
       }}
     />
