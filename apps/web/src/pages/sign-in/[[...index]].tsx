9805f9d648d9289e3f02fdc69c5ce05d03c13163
Miten Chauhan <miten@chauhan.nz>
2023-03-26 21:54:21 +1100

    Remove signup button from the dom when flag is disabled

--- apps/web/src/pages/sign-in/[[...index]].tsx
+++ apps/web/src/pages/sign-in/[[...index]].tsx
@@ -1,10 +1,14 @@
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import { SignIn } from "@clerk/nextjs";
+import { clsx } from "clsx";
+
+import { useFeatureFlag } from "@/hooks/feature-flag";
 
 const SignInPage: NextPage = () => {
   const { query } = useRouter();
   const redirectUrl = (query.redirect_url as string) ?? "/";
+  const { enabled: signUpEnabled } = useFeatureFlag("sign_up");
 
   return (
     <SignIn
@@ -12,6 +16,12 @@ const SignInPage: NextPage = () => {
       routing="path"
       signUpUrl="/sign-up"
       redirectUrl={redirectUrl}
+      appearance={{
+        elements: {
+          // NOTE: this doesn't remove the link from the DOM.
+          footerAction__signIn: clsx({ hidden: !signUpEnabled }),
+        },
+      }}
     />
   );
 };
