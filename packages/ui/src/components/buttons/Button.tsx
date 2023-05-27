9794774184266842b187bb4667956407fb9522cf
Miten Chauhan <miten@chauhan.nz>
2023-05-27 12:35:35 +1000

    Update button component

--- packages/ui/src/components/buttons/Button.tsx
+++ packages/ui/src/components/buttons/Button.tsx
@@ -14,6 +14,14 @@ export type HTMLButtonProps = DetailedHTMLProps<
 export const BUTTON_VARIANTS = ["solid", "outline", "ghost", "text"] as const;
 export type ButtonVariant = (typeof BUTTON_VARIANTS)[number];
 
+export const BUTTON_COLORS = ["primary", "black", "white"] as const;
+export type ButtonColor = (typeof BUTTON_COLORS)[number];
+
+export type ButtonStyle = {
+  color?: ButtonColor;
+  variant?: ButtonVariant;
+};
+
 export const BUTTON_ICON_POSITIONS = ["left", "right"] as const;
 export type ButtonIconPosition = (typeof BUTTON_ICON_POSITIONS)[number];
 
@@ -21,13 +29,37 @@ export const BUTTON_BASE_CLASSES = "rounded-md font-medium border";
 export const BUTTON_CLASSES = `${BUTTON_BASE_CLASSES} px-4 py-2`;
 export const ICON_BUTTON_CLASSES = `${BUTTON_BASE_CLASSES} p-2`;
 
-export const BUTTON_VARIANT_CLASSES: Record<ButtonVariant, string> = {
-  ghost: "",
-  outline: "border-amber-400 bg-transparent text-amber-400",
-  solid:
-    "bg-amber-400 border-amber-400 text-white dark:text-amber-800 dark:bg-amber-200 dark:border-amber-200",
-  text: "",
-} as const;
+export const BUTTON_STYLE_CLASSES: Record<
+  ButtonVariant,
+  Record<ButtonColor, string>
+> = {
+  ghost: {
+    primary: "text-amber-400",
+    black: "text-black",
+    white: "text-white",
+  },
+  outline: {
+    primary:
+      "text-amber-400 border-amber-400 bg-transparent dark:text-amber-200 dark:border-amber-200",
+    black:
+      "text-black border-black bg-transparent dark:text-white dark:border-white",
+    white:
+      "text-white border-white bg-transparent dark:text-black dark:border-black",
+  },
+  solid: {
+    primary:
+      "bg-amber-400 border-amber-400 text-white dark:text-amber-800 dark:bg-amber-200 dark:border-amber-200",
+    black:
+      "bg-black border-black text-white dark:text-black dark:bg-white dark:border-white",
+    white:
+      "bg-white border-white text-black dark:text-white dark:bg-black dark:border-black",
+  },
+  text: {
+    primary: "text-amber-400",
+    black: "text-black dark:text-white",
+    white: "text-white dark:text-black",
+  },
+};
 
 export const ICON_VARIANT_CLASSES: Record<ButtonVariant, string> = {
   ghost: "",
@@ -37,18 +69,19 @@ export const ICON_VARIANT_CLASSES: Record<ButtonVariant, string> = {
 } as const;
 
 export const getButtonClasses = (
-  variant: ButtonVariant = "solid",
+  { color = "primary", variant = "solid" }: ButtonStyle,
   hasIcon = false,
   ...rest: string[]
 ) => {
   return clsx(
     hasIcon ? ICON_BUTTON_CLASSES : BUTTON_CLASSES,
-    BUTTON_VARIANT_CLASSES[variant],
+    BUTTON_STYLE_CLASSES[variant][color],
     ...rest,
   );
 };
 
 export type ButtonProps = {
+  color?: ButtonColor;
   icon?: ReactElement;
   iconPosition?: ButtonIconPosition;
   variant?: ButtonVariant;
@@ -59,6 +92,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     const {
       children,
       className = "",
+      color,
       icon,
       iconPosition = "left",
       variant,
@@ -68,7 +102,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         ref={ref}
-        className={getButtonClasses(variant, !!icon, className)}
+        className={getButtonClasses({ color, variant }, !!icon, className)}
         {...rest}
       >
         {icon && iconPosition === "left" && <span>{icon}</span>}
