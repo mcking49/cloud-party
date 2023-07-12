6864e2d6322f284cb57e97db297aca5dc72d3b34
Miten Chauhan <miten@chauhan.nz>
2023-07-12 15:12:43 +1000

    Upgrade storybook

--- apps/workshop/.storybook/main.cjs
+++ apps/workshop/.storybook/main.cjs
@@ -1,19 +1,14 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-styling",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-styling", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
   features: {
-    storyStoreV7: true,
+    storyStoreV7: true
   },
   docs: {
-    autodocs: true,
-  },
+    autodocs: true
+  }
 };
