5b2de301b7e1d7a44eba632405619c2ed6f2c603
Miten Chauhan <miten@chauhan.nz>
2023-07-15 08:37:42 +1000

    Add empty state to home page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,21 +1,35 @@
+import { useRouter } from "next/router";
+
+import { IconEvent } from "@cloud-party/ui";
+
 import { api } from "@/utils/api";
+import { NoItemsWithCTA } from "@/components/empty-states";
 import DashboardLayout from "@/layouts/dashboard-layout";
 import { type NextPageWithLayout } from "./_app";
 
-const Web: NextPageWithLayout = () => {
+const HomePage: NextPageWithLayout = () => {
   const { data: myParties, isLoading } = api.party.allMyParties.useQuery();
+  const router = useRouter();
 
   if (isLoading) {
     return <p>Loading...</p>;
   } else if (!myParties || !myParties?.length) {
-    return <div>No parties found. Create one</div>;
+    return (
+      <NoItemsWithCTA
+        ctaLabel="New party"
+        icon={<IconEvent className="h-11 w-11 text-gray-400" />}
+        message="Get started by creating a new party."
+        title="No parties"
+        onClickCta={() => void router.push("/party/new")}
+      />
+    );
   }
 
   return <div></div>;
 };
 
-Web.getLayout = (page) => {
+HomePage.getLayout = (page) => {
   return <DashboardLayout>{page}</DashboardLayout>;
 };
 
-export default Web;
+export default HomePage;
